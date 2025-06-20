d2bbd9e022f2918dd2dce39df9b3835d1f8392e8
Vijay Thomas <103568167+realsukehiro@users.noreply.github.com>
2025-06-20 21:06:30 +0530

    Add files via upload

---
+++ My DSA Journey/Leetcode Problems/Easy/FrequencyElements.java
@@ -0,0 +1,36 @@
+package Easy;
+
+import java.util.HashMap;
+
+class FrequencyElements {
+    
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i < nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        int maxValue = Integer.MIN_VALUE;
+        for (int freq : map.values()) {
+            if (freq > maxValue) {
+                maxValue = freq;
+            }
+        }
+        int count = 0;
+        for (int freq : map.values()) {
+            if (freq == maxValue) {
+                count++;
+            }
+        }
+        return count * maxValue;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+
+        FrequencyElements fe = new FrequencyElements();
+
+        int result = fe.maxFrequencyElements(nums);
+
+        System.out.println(result);
+    }
+
+}
